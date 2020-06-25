29b7fe6cd53232b9958360c99bf12052c6735313
Raphaël <shadowraphi@gmail.com>
2020-06-25 21:36:47 +0200

    Modification architecture + redesign

---
+++ main.js
@@ -0,0 +1,25 @@
+document.write(`<header>
+    <h1 class="big-title">Annie Bonnecaze,</h1><h3 class="small-title">Artiste Peintre</h3>
+    <img src="assets/banner.png">
+</header>
+
+<nav>
+    <ul>                  
+      <li><a href="index.html">Accueil</a></li>
+
+      <li class="deroulant"><a href="pastels.html">Pastels</a>
+        <ul class="sous">
+          <li><a href="Pastel_personnages.html">Portraits et Animaux</a></li>
+          <li><a href="Pastel_Paysage.html">Paysages</a></li>
+        </ul>
+      </li>
+      <li class="deroulant"><a href="huiles.html">Huiles</a>
+        <ul class="sous">
+          <li><a href="huiles_Paysages.html">Paysages</a></li>
+          <li><a href="huiles_fleurs.html">Fleurs</a></li>
+        </ul>
+      </li>
+
+      <li><a href="#footer">Me contacter</a></li>
+    </ul>
+</nav>`);
