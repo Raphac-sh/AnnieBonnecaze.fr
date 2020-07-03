42bd556e8788fec45bc0f5e97977caa922d468a6
Raphaël <shadowraphi@gmail.com>
2020-07-03 18:38:36 +0200

    Mesures, ajustements

--- auto-update.js
+++ auto-update.js
@@ -8,16 +8,10 @@ document.write(`
     <ul>                  
       <li><a href="index.html">Accueil</a></li>
 
-      <li class="deroulant"><a href="#">Pastels</a>
+      <li><a href="Pastel.html">Pastel</a></li>
+      <li class="deroulant"><a href="#">Huiles </a>
         <ul class="sous">
-          <li><a href="pastels.html">Prix et Divers</a></li>
-          <li><a href="Pastel_personnages.html">Portraits et Animaux</a></li>
-          <li><a href="Pastel_Paysage.html">Paysages</a></li>
-        </ul>
-      </li>
-      <li class="deroulant"><a href="#">Huiles</a>
-        <ul class="sous">
-          <li><a href="huiles.html">Prix et Divers</a></li>
+          <li><a href="huiles.html">Divers</a></li>
           <li><a href="huiles_Paysages.html">Paysages</a></li>
           <li><a href="huiles_fleurs.html">Fleurs</a></li>
         </ul>
