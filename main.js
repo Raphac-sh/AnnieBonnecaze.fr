5efecee2749f620bba2c8c1f55b97cca2594f38a
Raphaël <shadowraphi@gmail.com>
2020-07-23 10:12:03 +0200

    Ajustement responsive, ajouts divers

--- main.js
+++ main.js
@@ -16,10 +16,10 @@ This file is part of AnnieBonnecaze.fr
 */
 
 class Carousel {
-  constructor(element, options = {slidesToScroll: 1,slidesVisible: 1}) {
+  constructor(element, options = { slidesToScroll: 1, slidesVisible: 1 }) {
     this.element = element;
     this.options = options;
-    
+
     this.currentItem = 0;
 
     this.isMobile = false;
@@ -27,15 +27,15 @@ class Carousel {
 
     // HTML architecture
     let children = [].slice.call(element.children);
-    this.root = this.createDiv("carousel");
-    this.container = this.createDiv("carousel__container");
+    this.root = this.createDiv('carousel');
+    this.container = this.createDiv('carousel__container');
     this.root.appendChild(this.container);
     this.element.appendChild(this.root);
 
-    this.items = children.map(child => {
-      let item = this.createDiv("carousel__item");
-      
-      item.appendChild(child)
+    this.items = children.map((child) => {
+      let item = this.createDiv('carousel__item');
+
+      item.appendChild(child);
       this.container.appendChild(item);
       return item;
     });
@@ -44,40 +44,38 @@ class Carousel {
     this.createNav();
 
     this.onResize();
-    window.addEventListener('resize', this.onResize.bind(this))
+    window.addEventListener('resize', this.onResize.bind(this));
   }
 
   createDiv(className) {
-    let div = document.createElement("div");
-    div.setAttribute("class", className);
+    let div = document.createElement('div');
+    div.setAttribute('class', className);
     return div;
   }
 
   // Gives good dimensions to elements
   setStyle() {
     let ratio = this.items.length / this.slidesVisible;
-    this.items.forEach(item => {
-      item.style.width = ((100 / this.slidesVisible) / ratio) + "%"
+    this.items.forEach((item) => {
+      item.style.width = 100 / this.slidesVisible / ratio + '%';
     });
-    this.container.style.width = (ratio * 100) + "%";
+    this.container.style.width = ratio * 100 + '%';
   }
 
   createNav() {
-    let nextButton = this.createDiv("carousel__next");
-    let prevButton = this.createDiv("carousel__prev");
+    let nextButton = this.createDiv('carousel__next');
+    let prevButton = this.createDiv('carousel__prev');
 
     this.root.appendChild(nextButton);
     this.root.appendChild(prevButton);
 
-    nextButton.addEventListener("click", this.next.bind(this));
-    prevButton.addEventListener("click", this.prev.bind(this));
-
+    nextButton.addEventListener('click', this.next.bind(this));
+    prevButton.addEventListener('click', this.prev.bind(this));
   }
 
   // Navigation methods
   next() {
     this.goToItem(this.currentItem + this.slidesToScroll);
-
   }
 
   prev() {
@@ -85,17 +83,20 @@ class Carousel {
   }
 
   goToItem(index) {
-
     //Check limits
-    if(index < 0) {
+    if (index < 0) {
       index = this.items.length - this.options.slidesVisible;
-    } else if (index >= this.items.length || (this.items[this.currentItem + this.options.slidesVisible] == undefined && index > this.currentItem)) {
+    } else if (
+      index >= this.items.length ||
+      (this.items[this.currentItem + this.options.slidesVisible] == undefined &&
+        index > this.currentItem)
+    ) {
       index = 0;
-      console.log("hey");
+      console.log('hey');
     }
 
     // animation and update
-    let translateX = index * -100 / this.items.length;
+    let translateX = (index * -100) / this.items.length;
     this.container.style.transform = 'translate3d(' + translateX + '%, 0, 0)';
     this.currentItem = index;
   }
@@ -103,13 +104,13 @@ class Carousel {
   onResize() {
     let mobile = window.innerWidth < 800;
     let tablet = window.innerWidth < 1200;
-    if(mobile != this.isMobile) {
+    if (mobile != this.isMobile) {
       this.isMobile = mobile;
-      this.setStyle()
+      this.setStyle();
     }
-    if(tablet != this.isTablet) {
+    if (tablet != this.isTablet) {
       this.isTablet = tablet;
-      this.setStyle()
+      this.setStyle();
     }
   }
 
@@ -123,13 +124,12 @@ class Carousel {
 }
 
 // Wait for DOM
-document.addEventListener("DOMContentLoaded", function () {
-
-  console.log("dom");
+document.addEventListener('DOMContentLoaded', function () {
+  console.log('dom');
 
   // Create carousel with parameters
-  let carousel = new Carousel(document.querySelector("#carousel1"), {
+  let carousel = new Carousel(document.querySelector('#carousel1'), {
     slidesToScroll: 1,
-    slidesVisible: 3
-  })
+    slidesVisible: 3,
+  });
 });
